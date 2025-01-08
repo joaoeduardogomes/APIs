b9fb8d6793455b3d51b9a7d9926ea9f88c03397d
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2025-01-07 21:46:42 -0300

    [add] put route and methods

--- books-management/handle_authors.py
+++ books-management/handle_authors.py
@@ -15,18 +15,33 @@ def execute_query(query, params=(), fetch=False) -> list:
         print(f"Database error: {e}")
         return None if not fetch else []
 
-
+#! GET
 def get_authors():
     query = "SELECT id, firstname || ' ' || lastname AS name FROM authors"
     rows = execute_query(query, fetch=True)
     
     if rows is not None:
-        columns = ["id", "firstname", "lastname"]
+        columns = ["id", "name"]
         return [dict(zip(columns, row)) for row in rows]
     return []
 
+#! GET
+def get_author_by_id(author_id) -> dict:
+    query = "SELECT id, firstname || ' ' || lastname AS name FROM authors WHERE id = ?"
+    row = execute_query(query, params=(author_id,), fetch=True)
+    
+    if row:
+        columns = ["id", "name"]
+        return dict(zip(columns, row[0]))
+    return {}
 
-
+#! POST
 def register_author(firstname, lastname):
     query = "INSERT INTO authors (firstname, lastname) VALUES (?, ?)"
     execute_query(query, params=(firstname, lastname))
+
+#! PUT
+def update_author(author_id, firstname, lastname) -> dict:
+    query = "UPDATE authors SET firstname = ?, lastname = ? WHERE id = ?"
+    execute_query(query=query, params=(firstname, lastname, author_id))
+    return get_author_by_id(author_id)
--- books-management/routes.py
+++ books-management/routes.py
@@ -3,11 +3,14 @@ import sqlite3
 import handle_authors  
 
 def register_routes(app):
+
+    #! GET
     @app.route("/authors", methods=["GET"])
     def get_authors():
         authors = handle_authors.get_authors()
         return jsonify(authors)
 
+    #! POST
     @app.route("/authors", methods=["POST"])
     def add_author():
         data = request.get_json()
@@ -20,6 +23,24 @@ def register_routes(app):
         except Exception as e:
             return jsonify({"error": f"Error: {e}"}), 400
 
+    #! GET
+    @app.route("/authors/<int:author_id>", methods=["GET"])
+    def get_author_by_id(author_id):
+        author = handle_authors.get_author_by_id(author_id)
+        return jsonify(author)
+    
+    #! PUT
+    @app.route("/authors/<int:author_id>", methods=["PUT"])
+    def update_author(author_id):
+        data = request.get_json()
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        
+        try:
+            updated_author = handle_authors.update_author(author_id, firstname, lastname)
+            return jsonify(updated_author)
+        except Exception as e:
+            return jsonify({"error": f"Error: {e}"}), 400
 
     """  @app.route("/")
     def index():
